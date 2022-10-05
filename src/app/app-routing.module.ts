544c090677b2a67c76679aa09e21e3e2ca351fda
josel19 <jjlugogomez123@gmail.com>
2022-10-05 01:31:17 -0500

    por terminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+const routes: Routes = [
 
-const routes: Routes = [];
+  {path: 'productos', component: ProductosComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'registro', component: RegistroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    ProductosComponent,
+    RegistroComponent,
+    
   ],
   imports: [
     BrowserModule,
